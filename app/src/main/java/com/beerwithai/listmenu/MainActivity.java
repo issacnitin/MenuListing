e0a6dbeee7fb77c34755b3deadc086c4cba370a3
Nitin Issac Joy <nitin.i.joy@gmail.com>
2017-09-13 22:53:10 +0530

    Reading from django

--- app/src/main/java/com/beerwithai/listmenu/MainActivity.java
+++ app/src/main/java/com/beerwithai/listmenu/MainActivity.java
@@ -1,10 +1,12 @@
 package com.beerwithai.listmenu;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +15,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import static com.beerwithai.listmenu.R.id.fab;
 
@@ -26,18 +41,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         st.add("abc");
         st.add("def");
-
-        String url ="10.0.2.2:8000/thanks";
-        
-        ArrayAdapter<String> arr = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, st);
+        String result="", url="http://10.0.2.2:8000/thanks/";
+        try {
+            result = new RetrieveFeedTask().execute(url).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d("Debug", result);
+        final String lines[] = result.split("  ");
+        ArrayAdapter<String> arr = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lines);
         ListView lt = (ListView)findViewById(R.id.lView);
         lt.setAdapter(arr);
         lt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), st.get(i), Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), lines[i], Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    class RetrieveFeedTask extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+
+        protected String doInBackground(String... urls) {
+            String output = "";
+            try {
+                URL url = new URL(urls[0]);
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(
+                                    url.openStream()));
+
+                    String inputLine;
+
+                    while((inputLine = in.readLine()) != null)
+                        output += inputLine;
+
+                    in.close();
+                return output;
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                return null;
+            }
+        }
+
     }
 
     @Override
